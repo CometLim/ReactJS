6a86989cb72d2defad514012f335038da0648276
LHJ <bluecomet0716@gmail.com>
2023-05-16 17:53:01 +0900

    fetch apis

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if ( toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-    
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [cost, setCost] = useState(1);
+  const [need, setNeed] = useState(1);
+  const onChange = (event) => {
+    setCost(event.target.value);
+    setNeed(1);
+  }
+
+  const handleInput = (event) => {
+    setNeed(event.target.value);
+  }   
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My ToDos ({toDos.length})</h1>
-      <form onSubmit ={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do"
-        />
-        <button>Add To Do</button>
-      </form>
-    </div>
-  );
-}
+      <h1>The coins!{loading ? "" :` Here are..${coins.length} coins`}</h1>
+      {loading ? <strong>loading...</strong> : <select onChange={onChange}>
+        <option>Select Coin!</option>
+        {coins.map((coin , index) =>
+        <option 
+          key={index} 
+          value={coin.quotes.USD.price} 
+          id={coin.symbol}
+          symbol = {coin.symbol} >
+          {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
+          </option>
+         )}
+      </select>}
+      <h2>Please enter the amount</h2>
+      <div>
+        <input type="number" value={need} onChange={handleInput} placeholder="dollor"/>$
+      </div>
+      <h2>You can get {need/cost}</h2>
+    </div>);
+  }
 
 export default App;
